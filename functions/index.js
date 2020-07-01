575bac19ab0719fb089216ec99a494e9698ca82a
steven <steven@burrows.plus.com>
2020-07-01 15:47:45 +0100

    add and get user profile details

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,13 @@ const functions = require("firebase-functions");
 const express = require("express");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require("./handlers/users");
 const FBAuth = require("./util/FBAuth");
 
 const app = express();
@@ -15,5 +21,7 @@ app.post("/scream", FBAuth, postOneScream);
 app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("europe-west3").https.onRequest(app);
